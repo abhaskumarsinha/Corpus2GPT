d4bca450752359c2bc9f74305c1ac28fa2f7fdbc
Abhas Kumar Sinha <31654395+abhaskumarsinha@users.noreply.github.com>
2024-04-01 15:26:59 +0530

    Let's see

--- models/embeddings.py
+++ models/embeddings.py
@@ -99,3 +99,17 @@ class PositionalEmbedding(keras.layers.Layer):
         y = self.embedding(keras.ops.einsum('bi, xi -> bi', ones, seq))
 
         return y
+
+
+class TokenAndPositionEmbedding(keras.layers.Layer):
+    def __init__(self, maxlen, vocab_size, embed_dim):
+        super().__init__()
+        self.token_emb = keras.layers.Embedding(input_dim=vocab_size, output_dim=embed_dim)
+        self.pos_emb = keras.layers.Embedding(input_dim=maxlen, output_dim=embed_dim)
+
+    def call(self, x):
+        maxlen = keras.ops.shape(x)[-1]
+        positions = keras.ops.arange(0, maxlen, 1)
+        positions = self.pos_emb(positions)
+        x = self.token_emb(x)
+        return x + positions

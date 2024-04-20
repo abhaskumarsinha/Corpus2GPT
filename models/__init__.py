b1fee267910a8658ec0e45e3f7d21f065871052f
Abhas Kumar Sinha <31654395+abhaskumarsinha@users.noreply.github.com>
2024-04-20 16:31:22 +0530

    It sucks, but its lit because it works

---
+++ models/__init__.py
@@ -0,0 +1,16 @@
+"""
+Models Package
+
+This package provides modules for building various neural network models.
+
+Submodules:
+- GPT: Contains the implementation of the GPT (Generative Pre-trained Transformer) model.
+- attention: Implements attention mechanisms used in neural network models.
+- decoder: Provides components for decoding sequences in neural network models.
+- embeddings: Contains modules for handling embeddings in neural network models.
+"""
+
+from models.GPT import *
+from models.attention import *
+from models.decoder import *
+from models.embeddings import *

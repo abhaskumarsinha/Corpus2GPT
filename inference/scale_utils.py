eb6a5ce421455953a05680f90635b69a1107239d
Abhas Kumar Sinha <31654395+abhaskumarsinha@users.noreply.github.com>
2024-04-20 15:48:18 +0530

    test

--- inference/scale_utils.py
+++ inference/scale_utils.py
@@ -15,9 +15,13 @@ def normalize_list(numbers):
 
     Formula:
         $x_i = \frac{x_i - x_{min}}{x_{min} - x_{max}}$
+        
         where,
+        
             $x_{min}$ is the smallest value of all $x_i$
+            
             $x_{max}$ is the largest value of all $x_i$
+            
             for all valid $i$.
     """
     min_val = min(numbers)

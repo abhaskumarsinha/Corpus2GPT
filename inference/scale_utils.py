006a3b02aaf23b7fc9a93ae2b0d4359ac4a4eb0d
Abhas Kumar Sinha <31654395+abhaskumarsinha@users.noreply.github.com>
2024-04-20 15:45:07 +0530

    LaTeX issues tbh

--- inference/scale_utils.py
+++ inference/scale_utils.py
@@ -14,10 +14,10 @@ def normalize_list(numbers):
         list of float: Normalized list of numbers.
 
     Formula:
-        $x_i = \frac{x_i - x_{\text{min}}}{x_{\text{min}} - x_{\text{max}}}$
+        $x_i = \frac{x_i - x_{min}}{x_{min} - x_{max}}$
         where,
-            $x_{\text{min}}$ is the smallest value of all $x_i$
-            $x_{\text{max}}$ is the largest value of all $x_i$
+            $x_{min}$ is the smallest value of all $x_i$
+            $x_{max}$ is the largest value of all $x_i$
             for all valid $i$.
     """
     min_val = min(numbers)

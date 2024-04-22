768d4fc1d382b9979e58d2eb9579de9c393811cf
Abhas Kumar Sinha <31654395+abhaskumarsinha@users.noreply.github.com>
2024-04-22 16:02:07 +0530

    docsssssss

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,30 @@
+"""
+Utilities Module
+===============
+
+This module contains utility classes and functions used within the C2G system.
+
+Submodules
+----------
+- config: Provides functionalities for managing configurations and modules within the C2G system.
+- utils: Contains the base class for C2G models and related utility functions.
+
+Classes
+-------
+- _C2G_CONFIG: Class for managing configurations and modules within the C2G system.
+- register_module: Decorator for registering modules within the C2G system.
+- BASE_C2G: Base class for C2G models.
+
+Examples
+--------
+Usage examples for the classes and functions provided in this module.
+
+"""
+
+from .config import _C2G_CONFIG, register_module
+from .utils import BASE_C2G
+
+
+
 from .config import *
 from .utils import *
